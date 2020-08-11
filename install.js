9c66b1f578b0d4be1ba99ba0ab54e56267bc01f5
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-08-11 13:55:50 -0600

    Update install.js

--- install.js
+++ install.js
@@ -13,20 +13,22 @@ function showInstallPromotion() {
   document.getElementById("installButton").style.display = "block";
 }
 
-document.getElementById("installBtn").addEventListener('click', (e) => {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
+function addInstallBtnListener(){
+  document.getElementById("installBtn").addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideMyInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
   });
-});
+}
 
 function hideInstallPromotion() {
   document.getElementById("installButton").style.display = "none";
