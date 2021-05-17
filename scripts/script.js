56c5d6cc1cc7aae193508c6ac576bd1c07a97b12
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2021-05-17 14:01:31 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
 window.onload = () => {
   alert('V1.0.0');
 }
+
+async function scan() {
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    log("> Scan started");
+    ndef.addEventListener("readingerror", () => {
+      log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      log(`> Serial Number: ${serialNumber}`);
+      log(`> Records: (${message.records.length,JSON.stringify(message.record)})`);
+    });
+  } catch (error) {
+    log("Argh! " + error);
+  }
+}
+
+async function write() {
+  try {
+    const ndef = new NDEFReader();
+    await ndef.write("Hello world!");
+    log("> Message written");
+  } catch (error) {
+    log("Argh! " + error);
+  }
+}
+
+function log(str) {
+  let span = document.createElement('SPAN');
+  span.textContent = str;
+  document.body.append(span);
+}
