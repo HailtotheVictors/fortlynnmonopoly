fd424bb5ac92c7cd546dd366a037fefb18b794c5
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2021-05-17 14:06:27 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  alert('V1.0.2');
+  alert('V1.0.3');
 }
 
 async function scan() {
@@ -12,7 +12,7 @@ async function scan() {
     });
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
       log(`> Serial Number: ${serialNumber}`);
-      log(`> Records: (${message.records.length,JSON.stringify(message.record)})`);
+      log(`> Records: (${message.records.length},${JSON.stringify(message.record)})`);
     });
   } catch (error) {
     log("Argh! " + error);
