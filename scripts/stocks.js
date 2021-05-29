945a5009ba49c689bd37c7496c00efb62c46e2dd
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2021-05-29 13:00:27 -0600

    Update stocks.js

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -1,7 +1,10 @@
 var stocks = [];
-stocks[0] = {name:'Pay Plus',color:'royalblue',price:150,dividend:75,resellValue:60,img:'pps',abbr:'PPS'};
-stocks[1] = {name:'QuinnAir',color:'limegreen',price:135,dividend:50,resellValue:54,img:'qar',abbr:'QAR'};
-stocks[2] = {name:'Leo\'s Gum',color:'orangered',price:120,dividend:40,resellValue:48,img:'lgum',abbr:'LGUM'};
-stocks[3] = {name:'The Bank of Carson',color:'white',price:110,dividend:30,resellValue:44,img:'tboc',abbr:'TBOC'};
-stocks[4] = {name:'Leo\'s Lottery',color:'forestgreen',price:100,dividend:25,resellValue:40,img:'leo',abbr:'LEO'};
-stocks[5] = {name:'The Fort Lynnian',color:'deeppink',price:75,dividend:20,resellValue:30,img:'tfl',abbr:'TFL'};
+stocks[0] = {name:'Pay Plus',color:'royalblue',price:200,dividend:100,img:'pps',abbr:'PPS'};
+stocks[1] = {name:'QuinnAir',color:'limegreen',price:175,dividend:70,img:'qar',abbr:'QAR'};
+stocks[2] = {name:'Leo\'s Gum',color:'orangered',price:150,dividend:50,img:'lgum',abbr:'LGUM'};
+stocks[3] = {name:'The Bank of Carson',color:'white',price:140,dividend:40,img:'tboc',abbr:'TBOC'};
+stocks[4] = {name:'Leo\'s Lottery',color:'forestgreen',price:120,dividend:30,img:'leo',abbr:'LEO'};
+stocks[5] = {name:'The Fort Lynnian',color:'deeppink',price:90,dividend:20,img:'tfl',abbr:'TFL'};
+stocks[6] = {name:'BreezeTV',color:'skyblue',price:85,dividend:17,img:'brz',abbr:'BRZ'};
+stocks[7] = {name:'Nightly News',color:'gainsboro',price:77,dividend:14,img:'ntn',abbr:'NTN'};
+stocks[8] = {name:'Capital Casino',color:'gold',price:60,dividend:10,img:'cc',abbr:'CC'};
