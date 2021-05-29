8c56ebf28182a921b11e2b0f1fb4b5b198f88f04
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2021-05-29 13:47:48 -0600

    Update stocks.js

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -1,8 +1,8 @@
 var stocks = [];
 stocks[0] = {name:'Pay Plus',color:'royalblue',price:200,dividend:100,img:'pps',abbr:'PPS'};
 stocks[1] = {name:'QuinnAir',color:'limegreen',price:175,dividend:70,img:'qar',abbr:'QAR'};
-stocks[2] = {name:'Leo\'s Gum',color:'orangered',price:150,dividend:50,img:'lgum',abbr:'LGUM'};
-stocks[3] = {name:'The Bank of Carson',color:'white',price:140,dividend:40,img:'tboc',abbr:'TBOC'};
+stocks[2] = {name:'The Bank of Carson',color:'white',price:150,dividend:50,img:'tboc',abbr:'TBOC'};
+stocks[3] = {name:'Leo\'s Gum',color:'orangered',price:140,dividend:40,img:'lgum',abbr:'LGUM'};
 stocks[4] = {name:'Leo\'s Lottery',color:'forestgreen',price:120,dividend:30,img:'leo',abbr:'LEO'};
 stocks[5] = {name:'The Fort Lynnian',color:'deeppink',price:90,dividend:20,img:'tfl',abbr:'TFL'};
 stocks[6] = {name:'BreezeTV',color:'skyblue',price:85,dividend:17,img:'brz',abbr:'BRZ'};
